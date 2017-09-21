57ea96c60babe1145ba60842adee92e6cc7bf7e9
wangshuai <632038284@qq.com>
2017-09-21 21:46:18 +0800

    实验

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,12 @@
 require.config({
 	baseUrl:"/public/assets",
 	paths:{
-		jquery:"jquery/jquery",
-		cookie:"jquery-cookie/jquery.cookie",
+		jquery : "jquery/jquery",
+		cookie : "jquery-cookie/jquery.cookie",
 		template : "artTemplate/template-web",
+		bootstrap:"bootstrap/js/bootstrap.min",
 		common:"../js/common",
 		login:"../js/login",
 		teacherlist : "../js/teacher-list"
-	}
-})
+	},
+});
--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(["jquery","template"],function($,template){
+define(["jquery","template","bootstrap"],function($,template){
 	// 调用接口获取所有的讲师数据
 	$.ajax({
 		type:"get",
@@ -9,7 +9,7 @@ define(["jquery","template"],function($,template){
 		  var html = template("tpl",{list:data.result});
 		  $("#teacher").html(html);
 
-		  $(".qyorjy").on("click",function(){
+		  $(".qyorjy").click(function(){
 		  	  var that = this;
 		  	  // 登录注销功能
 		  	  var td = $(this).closest('td');
@@ -24,14 +24,31 @@ define(["jquery","template"],function($,template){
 		  	  		if(data.code == 200){
                        td.attr("data-status",data.result.tc_status);
                        if(data.result.tc_status == 0){
-                           $(that).text("注销");
+                           $(that).html("注销");
                        }else{
-                       	   $(that).text("启用");
+                       	   $(that).html("启用");
                        }
 		  	  		}
 		  	  	}
-		  	  })
-		  })
+		  	  });
+		  });
+
+
+		 $(".chak").click(function(){
+		  	 var td = $(this).closest('td');
+		  	 var tcId = td.attr("data-tcId");
+		  	 $.ajax({
+		  	 	type:"get",
+		  	 	url:"/api/teacher/view",
+		  	 	data:{tc_id:tcId},
+		  	 	dataType:"json",
+		  	 	success:function(data){
+		  	 	   var html = template("chakTpl",data.result);
+		  	 	   $("#chakInfo").html(html);
+		  	 	   $("#teacherModal").modal();
+		  	 	}
+		  	 });
+		  });
 		}
-	})
-})
+	});
+});
