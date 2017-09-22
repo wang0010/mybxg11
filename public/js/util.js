e3f9eeb56bca4fb81a6bdb139bb824fa350806ab
wangshuai <632038284@qq.com>
2017-09-22 10:33:34 +0800

    实现添加和编辑讲师页面共用

---
+++ public/js/util.js
@@ -0,0 +1,21 @@
+define(["jquery"],function($){
+	return {
+		qs:function(key){
+		  var parm = location.search.substr(1);
+           // console.log(parm);
+		   var tcId = null;
+		   if(parm){
+		   	 var ps = parm.split("&");
+		   	 $.each(ps,function(index,item){
+		   	 	var kv = item.split("=");
+		   	 	// console.log(kv);
+		   	 	if(kv[0] == key){
+		   	 		tcId = kv[1];
+		   	 		return false;
+		   	 	}
+		   	 })
+		   }
+		   return tcId;
+		}
+	}
+})
