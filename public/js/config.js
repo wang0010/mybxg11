ce27428503058f710e59b387cd3140d885559963
wangshuai <632038284@qq.com>
2017-09-25 10:22:03 +0800

    头像bug 修复 添加富文本功能

--- public/js/config.js
+++ public/js/config.js
@@ -1,35 +1,39 @@
 require.config({
-	baseUrl:"/public/assets",
-	paths:{
-		jquery : "jquery/jquery",
-		cookie : "jquery-cookie/jquery.cookie",
-		template : "artTemplate/template-web",
-		bootstrap:"bootstrap/js/bootstrap.min",
-		datepicker:"bootstrap-datepicker/js/bootstrap-datepicker",
-        language:"bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min",
-        validate:"validate/jquery-validate",
-        form:"jquery-form/jquery.form",
-        uploadify:"uploadify/jquery.uploadify.min",
-        region:"jquery-region/jquery.region",
-		util:"../js/util",
-		common:"../js/common",
-		login:"../js/login",
-		teacherlist : "../js/teacher-list",
-		teacheradd:"../js/teacher-add",
-		settings:"../js/settings"
-	},
-	shim:{
-		bootstrap:{
-			deps:["jquery"]
-		},
-		language:{
-			deps:["jquery","datepicker"]
-		},
-		validate:{
-			deps:["jquery"]
-		},
-		uploadify:{
-			deps:["jquery"]
-		}
-	}
+  baseUrl : '/public/assets',
+  paths : {
+    jquery : 'jquery/jquery',
+    cookie : 'jquery-cookie/jquery.cookie',
+    template : 'artTemplate/template-web',
+    bootstrap : 'bootstrap/js/bootstrap.min',
+    datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
+    language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+    validate : 'validate/jquery-validate',
+    form : 'jquery-form/jquery.form',
+    uploadify : 'uploadify/jquery.uploadify.min',
+    region : 'jquery-region/jquery.region',
+    ckeditor : 'ckeditor/ckeditor',
+    util : '../js/util',
+    common : '../js/common',
+    login : '../js/login',
+    teacherlist : '../js/teacher-list',
+    teacheradd : '../js/teacher-add',
+    settings : '../js/settings'
+  },
+  shim : {
+    bootstrap : {
+      deps : ['jquery']
+    },
+    language : {
+      deps : ['jquery','datepicker']
+    },
+    validate : {
+      deps : ['jquery']
+    },
+    uploadify : {
+      deps : ['jquery']
+    },
+    ckeditor : {
+      exports : 'CKEDITOR'
+    }
+  }
 });
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(["jquery","template","region","uploadify"],function($,template){
+define(["jquery","template","ckeditor","uploadify","region","datepicker","language"],function($,template,CKEDITOR){
 	// 调用接口 获取个人信息
 	$.ajax({
 		type:"get",
@@ -9,17 +9,28 @@ define(["jquery","template","region","uploadify"],function($,template){
 		  var html  = template("personTpl",data.result);
 		  $("#personInfo").html(html);
 		  // 处理头像上传
-          $("#upfile").uploadify({
-          	swf:"/public/assets/uploadify/uploadify.swf",
-          	uploader:"api/uploader/avatar",
-          	fileObjName:"tc_avatar",
-          	onUploadSuccess:function(a,b){
-          		console.log(b)
-          	}
-          })
+			  $('#upfile').uploadify({
+		        width : 120,
+		        height : 120,
+		        buttonText : '',
+		        itemTemplate : '<span></span>',
+		        swf : '/public/assets/uploadify/uploadify.swf',
+		        uploader : '/api/uploader/avatar',
+		        fileObjName : 'tc_avatar',
+		        onUploadSuccess : function(a,b){
+		          var obj = JSON.parse(b);
+		          $('.preview img').attr('src',obj.result.path);
+		        }
+		      });
 		  // 处理省市县三级联动
 		 $("#pcd").region({
 		 	url:"/public/assets/jquery-region/region.json"
+		 });
+		 // 处理富文本
+		 CKEDITOR.replace("editor",{
+		 	toolbarGroups : [
+					{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+					{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },				]
 		 })
 		}
 	})
