e7fc29df4299096949d9f379b6fddc348a648da6
wangshuai <632038284@qq.com>
2017-09-25 19:59:47 +0800

    完成课程列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,15 @@
 define(["jquery","template","util"],function($,template,util){
    // 设置导航菜单选中
    util.setMenu(location.pathname);
+   // 请求数据 
+   $.ajax({
+   	 type:"get",
+   	 url:"/api/course",
+   	 dataType:"json",
+   	 success:function(data){
+   	 	// 解析页面 渲染数据
+   	 	var html = template("courseTpl",data);
+   	 	$("#courseInfo").html(html)
+   	 }
+   })
 })
