5c5500bce3bc75bceee38621f0a844a6c868eb90
wangshuai <632038284@qq.com>
2017-09-26 15:16:08 +0800

    完成封面上传功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,3 +1,32 @@
-define(["jquery","template","util"],function($,template,util){
+define(["jquery","template","util","uploadify"],function($,template,util){
     util.setMenu("/course/add")
-})
+    // 获取课程ID
+    var csId = util.qs("cs_id")
+    // 获取数据
+    $.ajax({
+    	type:"get",
+    	url:"/api/course/picture",
+    	data:{cs_id:csId},
+    	dataType:"json",
+    	success:function(data){
+    		var html = template("picTpl",data.result)
+    		$("#picInfo").html(html)
+    		// 处理上传封面
+    		$("#myfile").uploadify({
+    			buttonText:"选择图片",
+    			width:80,
+    			height:"auto",
+    			buttonClass:"btn btn-success btn-sm",
+    			itemTemplate:"<span></span>",
+    			swf :"/public/assets/uploadify/uploadify.swf",
+    			uploader:"/api/uploader/cover",
+    			fileObjName:"cs_cover_original",
+    			formData:{cs_id:csId}, //额外传递一个参数
+    		    onUploadSussess:function(a,b,c){
+    		    	var obj = JSON.parse(b.trim())
+                    $(".preview img").attr("src",obj.result.path)
+    		    } 
+    		})
+    	}
+    })
+})	
