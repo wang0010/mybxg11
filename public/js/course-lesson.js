78be75aca1e0d20bd30804c88a9346d703489686
wangshuai <632038284@qq.com>
2017-09-27 20:47:45 +0800

    完成课时添加和编辑的表单提交操作

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(["jquery","template","util","bootstrap"],function($,template,util){
+define(["jquery","template","util","bootstrap","form"],function($,template,util){
      // 设置导航栏
      util.setMenu("/course/add")
      // 获取ID
@@ -19,7 +19,22 @@ define(["jquery","template","util","bootstrap"],function($,template,util){
 		    // 解析数据 渲染页面
 			  var html = template("modalTpl",{operate:"课程添加"})
               $("#modalInfo").html(html) 
-     			$("#chapterModal").modal();
+     		  $("#chapterModal").modal();
+
+              // 添加数据
+             $("#addOrEdit").click(function(){
+             	$("#lessForm").ajaxSubmit({
+              	type:"post",
+              	url:"/api/course/chapter/add",
+              	data:{ct_cs_id:csId},
+              	dataType:"json",
+              	success:function(data){
+              		if(data.code == 200){
+              			location.reload();
+              		}
+              	}
+              })
+             })
      		})
 
      		// 课程编辑
@@ -38,6 +53,22 @@ define(["jquery","template","util","bootstrap"],function($,template,util){
      					data.result.operate = "课程编辑"
      					var html = template("modalTpl",data.result)
                         $("#modalInfo").html(html) 
+
+                        // 编辑数据
+					        $("#addOrEdit").click(function(){
+					     	$("#lessForm").ajaxSubmit({
+					      	type:"post",
+					      	url:"/api/course/chapter/modify",
+					      	data:{ct_cs_id:csId,ct_id:ctId},
+					      	dataType:"json",
+					      	success:function(data){
+					      		if(data.code == 200){
+					      			// 刷新页面
+					      			location.reload();
+					      		}
+					      	}
+					      })
+				       })
      				}
      			})
      			$("#chapterModal").modal();
